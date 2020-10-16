8ae5262bbf8fdb0204ccc1ff6b55dfc12d07d8c6
wpearson-web <pearson.willy@gmail.com>
2020-10-16 18:22:39 -0500

    added if statement to verify user entry 8-128

--- Develop/assets/javascript/script.js
+++ Develop/assets/javascript/script.js
@@ -1,33 +1,44 @@
-//if, else if boolean statements to determine which declared variable
-
-//function to randomly generate based on 
-
 // generate button - variables to store generated values
 var generateBtn = document.querySelector( '#generate' );
 var lowerCaseValue;
 var upperCaseValue;
 var numberCaseValue;
 var specCharValue;
-var passLengthValue;
-
+var passCountValue;
 
 // Write password to the #password input
 function writePassword() {
+
   var password = generatePassword();
   var passwordText = document.querySelector( '#password' );
-
   passwordText.value = password;
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener( 'click', writePassword );
+//generates password critera based on user prompts
+function generatePassword() {
+  var passCount = prompt( "How many characters would you like your password to be (8-128)?" );
+  //the final password parameters
+  var passcrtiera = [];
 
-//variable declaration and user prompts
-var createPassword = function () {
-  var charCount = prompt( "How many characters would you like your password to be?" );
-  var upperCase = confirm( "Please click 'OK' if you would like to have UPPER CASE letters in your generated password; Otherwise click cancel to continue." );
-  var lowerCase = confirm( "Please click 'OK' if you would like to have 'lower case' letters in your generated password; Otherwise click cancel to continue." );
-  var specChar = confirm( "Please click 'OK' if you would like to have special characters in your generated password; Otherwise click cancel to continue." );
-  var numChar = confirm( "Please click 'OK' if you would like to have numbers in your generated password; Otherwise click cancel to continue." )
-  console.log( charCount, upperCase, lowerCase, specChar, numChar );
-};
+  if ( passCountValue >= 8 && passCount <= 128 ) {
+    passCountValue = parseInt( passCount );
+
+    var upperCase = confirm( "Please click 'OK' if you would like to have UPPER CASE letters in your generated password; Otherwise click cancel to continue." );
+    var lowerCase = confirm( "Please click 'OK' if you would like to have 'lower case' letters in your generated password; Otherwise click cancel to continue." );
+    var specChar = confirm( "Please click 'OK' if you would like to have special characters in your generated password; Otherwise click cancel to continue." );
+    var numChar = confirm( "Please click 'OK' if you would like to have numbers in your generated password; Otherwise click cancel to continue." );
+    console.log( charCount, upperCase, lowerCase, specChar, numChar );
+  };
+
+  //lowercase criteria
+
+  //uppercase criteria
+
+  //numbercase criteria
+
+  //specChar criteria
+
+  //randonmly calculate passcriteria
+
+  // Add event listener to generate button
+  generateBtn.addEventListener( 'click', writePassword );
