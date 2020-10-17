f4b06d82195e49629236400c1cb60ed76d7c0337
wpearson-web <pearson.willy@gmail.com>
2020-10-17 16:15:39 -0500

    if/else statements and variable cleanup

--- Develop/assets/javascript/script.js
+++ Develop/assets/javascript/script.js
@@ -1,60 +1,112 @@
-// generate button - variables to store generated values
-var generateBtn = document.querySelector( '#generate' );
-var upperCaseValue;
-var lowerCaseValue;
-var numberCaseValue;
-var specCharValue;
-
-// Write password to the #password input
-
 //generates password critera based on user prompts
-pwd.reset();
 function writePassword() {
-  passCount = parseInt( prompt( "How many characters would you like your password to be (8-128)?" ) );
-  //the final password parameters
-  newPassword = [];
+  pwd.reset();
+  charCount = parseInt( prompt( "How many characters would you like your password to be (8-128)?" ) );
 
   //if user entered pass count is not 8-128
-  if ( !passCount ) {
+  if ( !charCount ) {
     alert( "Please enter a valid number between 8-128 if you would like to continue." );
-    //return generatePassword();
   }
-  else if ( passCount < 8 || passCount > 128 ) {
+  else if ( charCount < 8 || charCount > 128 ) {
     alert( "Please enter a valid number between 8-128." );
     return writePassword();
   }
   else {
-    upperCase = confirm( "Please click 'OK' if you want UPPERCASE letters in your generated password; Otherwise click cancel to continue." );
-    lowerCase = confirm( "Please click 'OK' if you want lowercase letters in your generated password; Otherwise click cancel to continue." );
-    specChar = confirm( "Please click 'OK' if you want $pec!al ch@r@cter$ in your generated password; Otherwise click cancel to continue." );
-    numChar = confirm( "Please click 'OK' if you want numb3rs in your generated password; Otherwise click cancel to continue." );
-    console.log( charCount, upperCase, lowerCase, specChar, numChar );
-  };
-
-  if ( !upperCase && !lowerCase && !numChar && !specChar );
-  passCriteria = alert( "Please select at least one criteria." )
-  return writePassword(); //TODO: verify workflow - how to handle loop of not entering any criteria
-}
-//newPassword declaration
-
-function generatePassword() {
-  if ( pwd.upperCase === true && !pwd.lowerCase && !pwd.numChar && !pwd.) {
-
+    pwd.upperCase = confirm( "Please click 'OK' if you want UPPERCASE letters in your generated password; Otherwise click cancel to continue." );
+    pwd.lowerCase = confirm( "Please click 'OK' if you want lowercase letters in your generated password; Otherwise click cancel to continue." );
+    pwd.specChar = confirm( "Please click 'OK' if you want $pec!al ch@r@cter$ in your generated password; Otherwise click cancel to continue." );
+    pwd.numChar = confirm( "Please click 'OK' if you want numb3rs in your generated password; Otherwise click cancel to continue." );
+    console.log( charCount, pwd.upperCase, pwd.lowerCase, pwd.specChar, pwd.numChar );
   }
 
+  if ( !pwd.upperCase && !pwd.lowerCase && !pwd.numChar && !pwd.specChar ) {
+    alert( "Please select at least one criteria." )
+    return writePassword();
+  }
 
+  function generatePassword() {
+    if ( pwd.specChar === true && pwd.lowerCase === true && pwd.upperCase === true && pwd.numChar === true ) {
+      newPassword - options.all;
+      console.log( options.all );
+    }
+    else if ( pwd.specChar === true && pwd.lowerCase === true && pwd.upperCase === true ) {
+      newPassword - options.slu;
+      console.log( options.slu );
+    }
+    else if ( pwd.specChar === true && pwd.lowerCase === true && pwd.numbers === true ) {
+      newPassword - options.sln;
+      console.log( options.sln );
+    }
+    else if ( pwd.specChar === true && pwd.upperCase === true && pwd.numChar === true ) {
+      newPassword - options.sun;
+      console.log( options.sun );
+    }
+    else if ( pwd.lowerCase === true && pwd.upperCase === true && pwd.numChar === true ) {
+      newPassword - options.lun;
+      console.log( options.lun );
+    }
+    else if ( pwd.specChar === true && pwd.lowerCase === true ) {
+      newPassword - options.sl;
+      console.log( options.sl );
+    }
+    else if ( pwd.specChar === true && pwd.upperCase === true ) {
+      newPassword - options.su;
+      console.log( options.su );
+    }
+    else if ( pwd.specChar === true && pwd.numChar === true ) {
+      newPassword - options.sn;
+      console.log( options.sn );
+    }
+    else if ( pwd.lowerCase === true && pwd.upperCase === true ) {
+      newPassword - options.lu;
+      console.log( options.lu );
+    }
+    else if ( pwd.lowerCase === true && pwd.numChar === true ) {
+      newPassword - options.ln;
+      console.log( options.ln );
+    }
+    else if ( pwd.upperCase === true && pwd.numChar === true ) {
+      newPassword - options.un;
+      console.log( options.un );
+    }
+    else if ( pwd.specChar === true && pwd.lowerCase === true && pwd.upperCase === true && pwd.numChar === true ) {
+      newPassword - options.lu;
+      console.log( options.lu );
+    }
+    else if ( pwd.specChar === true ) {
+      newPassword - specials;
+      console.log( specials );
+    }
+    else if ( pwd.lowerCase === true ) {
+      newPassword - charactersLower;
+      console.log( charactersLower );
+    }
+    else if ( pwd.upperCase === true ) {
+      newPassword - charactersUpper;
+      console.log( charactersUpper );
+    }
+    else if ( pwd.specChar === true ) {
+      newPassword - numbers;
+      console.log( numbers );
+    }
+    var password = generatePassword();
+    //the final password parameters
+    newPassword = [];
+    var passwordText = document.querySelector( '#password' );
+    passwordText.value = password;
+  }
 }
-var password = generatePassword();
-var passwordText = document.querySelector( '#password' );
-passwordText.value = password;
+
+// generate button - variables to store generated values
+var generateBtn = document.querySelector( '#generate' );
 
 //randonmly calculate passcriteria
-var passCount;
+var charCount;
 var pwd = {
-  upperCase = true,
-  lowerCase = true,
-  specChar = true,
-  numChar = true,
+  upperCase: true,
+  lowerCase: true,
+  specChar: true,
+  numChar: true,
   reset: function () {
     this.upperCase = true;
     this.lowerCase = true;
